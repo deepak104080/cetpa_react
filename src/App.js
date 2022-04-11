9da2c2250318dd6b2c20d8c8e1f67aa6d2f594cb
deepak104080 <deepak104080@gmail.com>
2022-04-11 12:25:39 +0530

    11th April 2022 - React Basics

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+// const temp = `<div class></div>`;
+
 function App() {
+  var location = 'GGN';
+
+  // class - css - className
+  // components - class and functional
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Altudo
+          Altudo - {location}
         </p>
         <a
           className="App-link"
@@ -23,3 +30,37 @@ function App() {
 }
 
 export default App;
+
+
+
+
+// JSX - JS + HTML / Templating Mechanism (Angular, Express, Laravel, )
+
+
+// Virtual DOM
+
+// obj - one orginal copy of dom
+
+
+// re-render component
+// obj2 - another copy of dom
+// shallow comparison and deep comparison
+
+// list of changes - node
+//render to html - implement it on actual dom
+
+// document.getElementById.value = new value
+
+
+// SPA
+
+
+// Type of Component : -
+// Class Component - class - React Features
+// Functional Component - function - static html
+
+
+
+
+
+// Till React 16.7 (16.8 - Hooks (jugad of react features))
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Home extends React.Component {
+    // constructor() {
+    //     super();
+    //     console.log('constructor of class...')
+    // }
+    render() {
+        return(
+            <div>Altudo - Class Component</div>
+        )
+    }
+}
+
+export default Home;
